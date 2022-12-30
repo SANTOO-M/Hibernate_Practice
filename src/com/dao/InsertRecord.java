55502472a1f7e425c32b0005024220a3e64a6739
Santhoshkumar_M <santoo1234abcd@gmail.com>
2022-12-30 14:06:27 +0530

    First Commit

---
+++ src/com/dao/InsertRecord.java
@@ -0,0 +1,31 @@
+package com.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.model.Customer;
+
+
+public class InsertRecord {
+
+	public static void main(String[] args) {
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=session.beginTransaction();
+		Customer customerObj=new Customer();
+		customerObj.setCustomerId(1000);
+		customerObj.setCustomerName("Santoo");
+		customerObj.setAddress("VOC Street");
+		customerObj.setCity("Salem");
+		customerObj.setState("TamilNadu");
+		customerObj.setPincode(636203);
+		session.save(customerObj);
+		tx.commit();
+		System.out.println("Record Successfully inserted...");
+		session.close();
+	}
+}
